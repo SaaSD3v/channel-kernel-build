44e9708ce20601ad3eccdaeb7fc91d9a5e992a8d
SaaSD3v <hearesaas@gmail.com>
2026-07-18 20:47:52 -0300

    Optimize anykernel.sh for recovery-based flashing

--- anykernel.sh
+++ anykernel.sh
@@ -5,17 +5,12 @@
 ## shell variables
 kernel.string=Channel Kernel for Moto G7 Play
 do.devicecheck=0
-block.string=/dev/block/bootdevice/by-name/boot;
+block.string=/dev/block/bootdevice/by-name/boot
 is_slot_device=0
-ramdisk.compression=auto
+ramdisk.compression=gzip
 patch.vbmeta.flag=auto
 patch.type=boot
-patch.flash=auto;
-
-## AnyKernel file attributes
-is_slot_device=0
-ramdisk.compression=auto
-patch.vbmeta.flag=auto
+patch.flash=auto
 
 ## AnyKernel install
 split_boot;
