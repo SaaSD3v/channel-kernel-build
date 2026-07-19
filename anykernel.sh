56ed3140ec470614e63231870029e33fc0596ae0
SaaSD3v <hearesaas@gmail.com>
2026-07-18 21:37:48 -0300

    Fix anykernel.sh: add . tools/ak3-core.sh import and correct variable format

--- anykernel.sh
+++ anykernel.sh
@@ -1,19 +1,38 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
-## AnyKernel setup
-## shell variables
-kernel.string=Channel Kernel for Moto G7 Play
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=Channel Kernel for Moto G7 Play (channel)
 do.devicecheck=0
-block.string=/dev/block/bootdevice/by-name/boot
-is_slot_device=0
-ramdisk.compression=gzip
-patch.vbmeta.flag=auto
-patch.type=boot
-patch.flash=auto
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=channel
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
 
-## AnyKernel install
-split_boot;
+
+### AnyKernel install
+# boot shell variables
+BLOCK=/dev/block/bootdevice/by-name/boot;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+split_boot; # skip ramdisk unpack since we are only flashing kernel
+
 flash_boot;
-
-## AnyKernel end
+## end boot install
