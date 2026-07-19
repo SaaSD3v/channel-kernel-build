46191cb1191e47436c13be4bd4c27e6996744e06
SaaSD3v <hearesaas@gmail.com>
2026-07-19 00:59:49 -0300

    update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,35 +4,26 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Channel Kernel for Moto G7 Play (channel)
-do.devicecheck=0
+kernel.string=Channel Kernel
+do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=channel
-device.name2=
-device.name3=
-device.name4=
-device.name5=
-supported.versions=
-supported.patchlevels=
-supported.vendorpatchlevels=
 '; } # end properties
 
-
-### AnyKernel install
 # boot shell variables
-BLOCK=/dev/block/bootdevice/by-name/boot;
-IS_SLOT_DEVICE=0;
-RAMDISK_COMPRESSION=none;
+BLOCK=auto;
+IS_SLOT_DEVICE=auto;
+RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
 # boot install
-split_boot; # skip ramdisk unpack since we are only flashing kernel
+dump_boot;
 
-flash_boot;
+write_boot;
 ## end boot install
